19ea700e10e1da2f774aac86351f706038415364
Saad Khilji <25100168@lums.edu.pk>
2025-06-15 19:41:43 +0500

    Fixed index.js issue

--- backend/index.js
+++ backend/index.js
@@ -16,5 +16,5 @@ app.use("/api/products", require("./routes/productRoutes"));
 app.use("/api/wishlist", require("./routes/wishlistRoutes"));
 app.use("/api/reviews", require("./routes/reviewRoutes"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
